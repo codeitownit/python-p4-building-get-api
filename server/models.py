1e2cc2e9ba8380061b8a9802443ac07093aabef6
Everlyn <gachuieve32@gmail.com>
2024-01-11 18:01:33 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -1,14 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+# from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
+
+    serialize_rules = ('-reviews.game',)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -23,8 +26,10 @@ class Game(db.Model):
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
+
+    serialize_rules = ('-game.reviews', '-user.reviews',)
     
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
@@ -40,6 +45,8 @@ class Review(db.Model):
 
 class User(db.Model):
     __tablename__ = 'users'
+
+    serialize_rules = ('-reviews.user',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
